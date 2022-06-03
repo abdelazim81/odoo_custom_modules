d7ac59762631578fdf234da99e328b2cfc52fb40
abdelazim81 <abdomido156@gmail.com>
2022-06-03 14:26:19 +0200

    add new item to selection to particular poition

--- om_hospital/models/product.py
+++ om_hospital/models/product.py
@@ -3,6 +3,9 @@ from odoo import api, models, _, fields
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
-
-    detailed_type = fields.Selection(selection_add=[('test', 'Test')],
+    #  if new item before x item add new item then x item
+    # if new item after x item add x item then new item
+    detailed_type = fields.Selection(selection_add=[('test', 'Test'),
+                                                    ('service',)
+                                                    ],
                                      ondelete={'test': 'cascade'})
